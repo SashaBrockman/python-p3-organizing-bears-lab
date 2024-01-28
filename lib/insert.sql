1aa9095b5e35f8b9866624ca04f0a9bcc527d7ac
Sasha <georgefbrockman@gmail.com>
2024-01-28 15:33:15 -0600

    finishes lab

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 12, 'M', 'Brown', 'Aggressive', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 8, 'M', 'Brown', 'Calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 3, 'F', 'Black', 'Loving', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 38, 'M', 'Brown', 'Aggressive', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 15, 'F', 'White', 'Sleepy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 58, 'M', 'Green', 'Moody', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 48, 'F', 'Brown', 'Proudy', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, 'F', 'Grey', 'Playful', 1);
